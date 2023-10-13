1ea6e6bff7e46ce390fca6dab24c9172e66b349f
elenpi <elnefpi@gmail.com>
2023-10-13 16:59:51 +0300

    Adding index.html

--- tickets/urls.py
+++ tickets/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="landing-page"),
+    path("create-ticket", views.create_ticket, name="ticket-creation-page"),
     path("tickets", views.tickets, name="tickets-page"),
     path("tickets/<slug:slug",views.ticket_detail, name="ticket-detail-page")
 ]
--- tickets/views.py
+++ tickets/views.py
@@ -4,10 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World!") 
+    return render(request, "tickets/index.html") 
+
+def create_ticket(request):
+    return HttpResponse("New ticket!") 
 
 def tickets(request):
-    return HttpResponse("My tickets!") 
+    return HttpResponse("My tickets!")
 
 def ticket_detail(request):
     return HttpResponse("A ticket!") 
