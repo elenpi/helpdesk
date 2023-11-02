b80c885d284ff4fce3d91f8d250597843326b701
elenpi <elnefpi@gmail.com>
2023-11-02 14:58:12 +0200

    Added Registration and view based on role

--- tickets/forms.py
+++ tickets/forms.py
@@ -1,5 +1,30 @@
 from django import forms
 from .models import Ticket
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "first_name", "last_name", "email", "password1", "password2")
+        help_texts = {
+            'username': None,
+            'password1': None,
+            'password2': None,
+        }
+
+    def save(self, commit=True):
+        user = super(CustomUserCreationForm, self).save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
 
 class CreateTicket(forms.ModelForm):
      class Meta:
--- tickets/views.py
+++ tickets/views.py
@@ -1,22 +1,38 @@
+from django.db.models.query import QuerySet
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.views import View
-from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import FormView,CreateView
 
-from .forms import CreateTicket
+from .forms import CreateTicket, CustomUserCreationForm
 from .models import Ticket
 
 
 # Create your views here.
 
+class Registration(FormView):
+    template_name = "registration/register.html"
+    form_class = CustomUserCreationForm
+    success_url = "/login"
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
 class OpenTicketsList(LoginRequiredMixin, ListView):
     template_name = "tickets/index.html"
     model = Ticket
     context_object_name= "tickets"
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.profile.is_agent:
+            return Ticket.objects.filter(assignee=user)
+        return Ticket.objects.filter(reporter=user)
+
 
 class CreateTicketView(LoginRequiredMixin, CreateView):
     model = Ticket
